e7af4143427aa4bf4076b92f3ea7b32f4fd849f5
holliemaethomas <holliemaethomas85@gmail.com>
2018-05-20 14:37:33 -0700

    creating objects in js

--- js/app.js
+++ js/app.js
@@ -37,3 +37,37 @@ function cookiesByHourPike() {
   totalSalesPike.innerHTML = itemContent + pike.totalDailySales;
 }
 cookiesByHourPike();
+///////////////////////comment line so i dont get confused/////////////////////////
+// create the store 
+var seatac = {
+  minCustomer: 3,
+  maxCustomer: 24,
+  avgerageSales: 1.2,
+  hoursOfOperation: ['06:00 ', '07:00 ', '08:00 ', '09:00 ', '10:00 ', '11:00 ', '12:00 ', '13:00 ', '14:00 ', '15:00 ', '16:00 ', '17:00 ', '18:00 ', '19:00 ', '20:00 '],
+  hourlySales: [],
+  totalDailySales: 0,
+  getRandom: function (minCustomer, maxCustomer) {
+    return Math.random() * (maxCustomer - minCustomer) + minCustomer;
+  },
+};
+
+//create the store function 
+
+function cookiesByHourSeatac() {
+  for (var i = 0; i < seatac.hoursOfOperation.length; i++) {
+    var randomHourlyNumber = Math.round(seatac.getRandom(seatac.minCustomer, seatac.maxCustomer) * seatac.avgerageSales);
+    seatac.hourlySales.push(randomHourlyNumber);
+    seatac.totalDailySales += seatac.hourlySales[i];
+    var newEl = document.createElement('li');
+    var newText = document.createTextNode(seatac.hoursOfOperation[i] + seatac.hourlySales[i] + ' cookies');
+    newEl.appendChild(newText);
+    var position = document.getElementById('seatac-list');
+    position.appendChild(newEl);
+  }
+
+  // add it together
+  var totalSalesSeatac = document.getElementById('seatac-total');
+  var itemContent = totalSalesSeatac.innerHTML;
+  totalSalesSeatac.innerHTML = itemContent + pike.totalDailySales;
+}
+cookiesByHourSeatac();
